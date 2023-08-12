62c827255f8008c5aa085b010fe77452df863e8d
Tanbir Ahammed <tanbirahammed789@gmail.com>
2023-08-12 15:08:59 +0600

    Add files via upload

---
+++ javahomework_3/src/main/java/com/tanbir/javahomework_3/Javahomework_3.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.tanbir.javahomework_3;
+
+/**
+ *
+ * @author TANBIR
+ */
+public class Javahomework_3 {
+
+    public static void main(String[] args) {
+       Javahomework_3 obj1 = new Javahomework_3();
+       String name = obj1.getName();
+       System.out.println(name);
+    }
+    public String getName(){
+        return "Tanbir";
+    }
+}
