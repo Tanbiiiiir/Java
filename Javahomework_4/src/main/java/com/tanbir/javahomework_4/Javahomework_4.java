2ded61770e7d88dc330693ee7d676abd586fb6b2
Tanbir Ahammed <tanbirahammed789@gmail.com>
2023-08-12 15:26:52 +0600

    Add files via upload

---
+++ Javahomework_4/src/main/java/com/tanbir/javahomework_4/Javahomework_4.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.tanbir.javahomework_4;
+
+/**
+ *
+ * @author TANBIR
+ */
+public class Javahomework_4 {
+
+    public static void main(String[] args) {
+        Javahomework_4 obj1 = new Javahomework_4();
+        String name = obj1.getName();
+        obj1.printAddress();
+        System.out.println(name);
+        
+    }
+    public String getName(){
+        return "Tanbir";
+    }
+    public void printAddress(){
+        System.out.println("Address is Gobindagonj");
+    }
+}
