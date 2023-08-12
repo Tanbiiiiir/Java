dcde741b915577f1223db076014235f3b1729076
Tanbir Ahammed <tanbirahammed789@gmail.com>
2023-08-12 14:55:31 +0600

    Add files via upload

---
+++ javahomework_2/src/main/java/com/tanbir/javahomework_2/Javahomework_2.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.tanbir.javahomework_2;
+
+/**
+ *
+ * @author TANBIR
+ */
+public class Javahomework_2 {
+
+    public static void main(String[] args) {
+        String name = getName();
+        System.out.println(name);
+        System.out.println("My first Java program.");
+    }
+    public static String getName(){
+        return "Tanbir";
+    }
+}
---
+++ javaproject_1/src/main/java/com/tanbir/javaproject_1/Javaproject_1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.tanbir.javaproject_1;
+
+/**
+ *
+ * @author TANBIR
+ */
+public class Javaproject_1{
+    public static void main(String[]args){
+    System.out.println(getName());
+    System.out.println("My first Java program");
+    }
+    public static String getName() {
+        return "Tanbir";
+    }
+}
