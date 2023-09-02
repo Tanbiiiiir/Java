3ca4f372ee58e78af7a800101b4f795a3f72a599
Tanbir Ahammed <tanbirahammed789@gmail.com>
2023-09-02 21:03:56 +0600

    Add files via upload

---
+++ NarrowingCasting.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tanbir.basic;
+
+/**
+ *
+ * @author TANBIR
+ */
+public class NarrowingCasting {
+    public static void main(String[] args) {
+        int i = 30;
+        short s = (short) i;
+        System.out.println("Value of i:" + i);
+        System.out.println("Value of s:"+ s);
+    }
+}
---
+++ WideningTypeCasting.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tanbir.basic;
+
+/**
+ *
+ * @author TANBIR
+ */
+public class WideningTypeCasting {
+    
+    public static void main(String[] args) {
+        byte b = 127;
+        short s = b;
+        System.out.println("Value of b: " + b);
+        System.out.println("VAlue of s: " + s);
+    }
+    
+}
