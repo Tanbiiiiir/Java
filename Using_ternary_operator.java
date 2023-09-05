6e7d680d8232e68f969273f739a55bbc5571266d
Tanbir Ahammed <tanbirahammed789@gmail.com>
2023-09-05 21:53:10 +0600

    Add files via upload

---
+++ Using_ternary_operator.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tanbir.basic;
+
+
+public class Using_ternary_operator {
+    public static void main(String[] args) {
+        int age = 18;
+        System.out.println(age >= 18? "voter" : "Not voter");
+    }
+}
