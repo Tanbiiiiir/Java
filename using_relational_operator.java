48810bf21003a5b5ac8523c9eb92a584da33f536
Tanbir Ahammed <tanbirahammed789@gmail.com>
2023-09-05 20:46:26 +0600

    Add files via upload

---
+++ using_relational_operator.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tanbir.basic;
+
+/**
+ *
+ * @author TANBIR
+ */
+public class using_relational_operator {
+    
+    public static void main(String[] args) {
+        int mark = 111;
+        
+        if (mark > 100){
+            System.out.println("The number is invalid.");
+        }
+        
+        else if(mark == 100){
+            System.out.println("You got full mark.");
+        }
+        
+        else if(mark >= 80){
+            System.out.println("A+");
+        }
+        
+        else if(mark >= 70){
+            System.out.println("A");
+        }
+        
+        else if(mark >= 60){
+            System.out.println("B+");
+        }
+        
+        
+        
+    }
+}
